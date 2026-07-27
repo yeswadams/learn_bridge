b4f0b4da056ccde7c79dbd6fadd786ccb68f2e71
yeswadams <yeswaadams73@gmail.com>
2026-07-27 15:36:26 +0300

    implemented the auth state using ChangeNotifier

--- mobile/lib/app.dart
+++ mobile/lib/app.dart
@@ -1,2 +1,22 @@
 import 'package:flutter/material.dart';
-import 'featu'
+import 'core/routing/app_router.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      title: "Learn Bridge",
+      theme: ThemeData.dark(useMaterial3: true).copyWith(
+        scaffoldBackgroundColor: const Color(0xFF121212),
+        colorScheme: const ColorScheme.dark(
+          primary: Color(0xFF7C65C1),
+          secondary: Color(0xFF03DAC6),
+        ),
+      ),
+      routerConfig: appRouter,
+    );
+  }
+}
