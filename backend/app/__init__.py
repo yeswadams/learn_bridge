03f87a24c86e5d279810dc13adb6e8814bd23684
yeswadams <yeswaadams73@gmail.com>
2026-07-30 20:01:59 +0300

    Implemented the programs API url endpoint using Blueprint

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,7 +1,17 @@
+# Application Factory
+
 from flask import Flask
+from app.features.programs.routes import programs_bp
+from app.features.users.routes import users_bp
 
 def create_app():
     app = Flask(__name__)
+
+    app.register_blueprint(
+        programs_bp, 
+        users_bp,
+        url_prefix="/api/vi/programs"
+    )
 
     @app.get('/')
     def index():
