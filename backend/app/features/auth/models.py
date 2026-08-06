f09416e037a9e4a89abc880aca42972ae8bc31bf
yeswadams <yeswaadams73@gmail.com>
2026-08-06 19:17:30 +0300

    added the user model using sqlalchemy

---
+++ backend/app/features/auth/models.py
@@ -0,0 +1,43 @@
+from datetime import datetime, timezone
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Integer, DateTime
+from app.extensions.database import db
+from typing import Optional
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True) 
+    full_name: Mapped[str] = mapped_column(
+        String(120), 
+        nullable=False
+    )
+    email: Mapped[str] = mapped_column(
+        String(255), 
+        unique=True, 
+        nullable=False
+    )
+    _password: Mapped[str] = mapped_column(
+        String(255), 
+        nullable=False
+    )
+    role: Mapped[str] = mapped_column(
+        String(50), 
+        default="student", 
+        nullable=False
+    )
+    age: Mapped[int] = mapped_column(
+        Integer, 
+        nullable=False
+    )
+    profile_img: Mapped[Optional[str]] = mapped_column(
+        String(500), 
+        nullable=True
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), 
+        default=lambda: datetime.now(timezone.utc)
+    )
+
+    #Relationships:
+    enrollements: Mapped[]
