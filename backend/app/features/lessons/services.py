5f859f01f60c20692ce797bb3db53b5c92067d45
yeswadams <yeswaadams73@gmail.com>
2026-08-15 22:10:03 +0300

    started the implementation of the lessons service

---
+++ backend/app/features/lessons/services.py
@@ -0,0 +1,12 @@
+from typing import Any, Optional, Dict
+
+class LessonService:
+    def __init__(self, db_session, course_id):
+        self.db = db_session
+        self.course = course_id
+
+    def create_course(self, instructor: str, course_id: str):
+        return lesson
+    
+        
+    
